f50508ba9acaea4210faab5511e46e2af05745a2
vibhanshu007 <vibhanshumail849@gmail.com>
2016-12-12 14:22:21 +0530

    Android Material design Singup and login example.

--- app/src/main/java/com/vibsbro/signupandloginexp/DbHelper.java
+++ app/src/main/java/com/vibsbro/signupandloginexp/DbHelper.java
@@ -1,8 +1,11 @@
 package com.vibsbro.signupandloginexp;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by root on 9/12/16.
@@ -10,6 +13,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DbHelper extends SQLiteOpenHelper {
 
+    public static final String TAG = DbHelper.class.getSimpleName();
     public static final String DB_NAME = "singup_login.db";
     public static final String DB_USER_TABLE = "user";
     public static final String DB_COL_NAME = "_name";
@@ -34,4 +38,35 @@ public class DbHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
     }
+
+    public void addUser(String email, String password){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(DB_COL_EMAIL,email);
+        values.put(DB_COL_PASSWORD,password);
+
+        long id = db.insert(DB_USER_TABLE,null,values);
+        db.close();
+        Log.d(TAG,"userInsert" +id);
+
+    }
+
+    public boolean getUser(String email,String pass){
+
+        String selectQueary = "select * from" + DB_USER_TABLE + " where " + DB_COL_EMAIL + "=" + "'"
+                + email + "'" +  "and" + DB_COL_PASSWORD + "=" + "'" +pass+ "'";
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQueary,null);
+        cursor.moveToFirst();
+        if (cursor.getCount()>0){
+
+            return true;
+        }
+        cursor.close();
+        db.close();
+        return false;
+    }
+
 }
